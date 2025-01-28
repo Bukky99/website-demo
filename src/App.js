d9efba92d65322e68c93847d2da219f9b5c8023e
Bukky99 <iloveherseys@gmail.com>
2025-01-28 07:17:25 -0600

    Created demo website page and passed in children page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageWrapper from './components/PageWrapper';
+import Home from './components/Pages/Home';
+
+function App() {
+  return (
+    <PageWrapper>
+      <Home />
+      <div>Hello!</div>
+    </PageWrapper>
+  );
+}
+
+export default App;
