50884af70798fcbd5da411132b2b0c1fe9799623
Bukky99 <iloveherseys@gmail.com>
2025-01-30 07:51:13 -0600

    Install react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <PageWrapper>
-      <Home />
+      <Router>
+        <Routes>
+          <Route 
+          path="/home"
+          component={Home}
+        />
+        </Routes>
+      </Router>
     </PageWrapper>
   );
 }
