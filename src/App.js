2a4d339f724da406c5e7531b36689bb2f0bf98a4
Bukky99 <iloveherseys@gmail.com>
2025-01-30 08:45:01 -0600

    Created about page route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
-import Home from './components/Pages/Home';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+//Pages
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+
 
 function App() {
   return (
-    <PageWrapper>
-      <Router>
-        <Routes>
-          <Route 
-            path="/home"
-            element={<Home />}
-          />
-        </Routes>
-      </Router>
-    </PageWrapper>
+    <Router>
+      <PageWrapper>
+          <Routes>
+            <Route 
+              exact={true}
+              path="/"
+              element={<Home />}
+            />
+
+            <Route
+              path="/about"
+              element={<About />}
+            />
+
+          </Routes>
+      </PageWrapper>
+    </Router>
   );
 }
 
